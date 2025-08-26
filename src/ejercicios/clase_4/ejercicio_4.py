24be681dff4fe2d14ddfe0a0337b87ad91c534ae
Alex-Soph <A01825459@tec.mx>
2025-08-26 08:33:58 -0600

    Update ejercicio_4.py
    ejercicio extra

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,26 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+#Alexhia Sophia Pérez Escobar || A01825459
+
+# Escribe un programa que pida un número al usuario. 
+# Si el número es menor a 0 deberá imprimir negativo
+# Si es igual a 0 deberá imprimir cero
+# Si es mayor a 0 deberá imprimir positivo
+
+print("\t<<-----  NEGATIVO O POSITIVO----->>")
+print("¡Bienvenido!")
+print()
+num = float (input("Por favor, ingresa un número: "))
+
+print()
+
+if num==0:
+         print("Cero")
+elif(num>0):
+    print("Positivo")
+else:
+    print("Negativo")
+
+print("\t<<---------- ¡Gracias! ---------->>")
+
